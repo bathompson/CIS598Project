3daf73d065013cc37c389655efcade41668fd7b0
bathompson <bathompson@ksu.edu>
2022-02-17 15:07:48 -0600

    Fixed issue with Registers struct

--- src/CPU/CPU.h
+++ src/CPU/CPU.h
@@ -13,8 +13,8 @@ typedef struct registers
     {
         struct
         {
-            uint8_t A;
             uint8_t F;
+            uint8_t A;
         };
         uint16_t AF;
     };
@@ -23,8 +23,8 @@ typedef struct registers
     {
         struct
         {
-            uint8_t B;
             uint8_t C;
+            uint8_t B;
         };
         uint16_t BC;
     };
@@ -33,8 +33,8 @@ typedef struct registers
     {
         struct
         {
-            uint8_t D;
             uint8_t E;
+            uint8_t D;
         };
         uint16_t DE;
     };
@@ -43,8 +43,8 @@ typedef struct registers
     {
         struct
         {
-            uint8_t H;
             uint8_t L;
+            uint8_t H;
         };
         uint16_t HL;
     };
